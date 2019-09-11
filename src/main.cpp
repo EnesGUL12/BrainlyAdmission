98e1033bb4f242c36969d6e18fd0af40b5132cf0
Enes <enes515254@gmail.com>
2019-09-11 17:44:43 +0600

    Added and managed RFID

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@
 #include <OneButton.h>
 #include <MFRC522.h>
 
-#define SS_PIN 12
-#define RST_PIN 14
+#define SS_PIN 15
+#define RST_PIN 0
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
 OneButton button(16, false);
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522 rfid(SS_PIN, RST_PIN);
 
+byte code;
+String id_str; 
 
 
 void click1(){
@@ -25,25 +27,36 @@ void click1(){
 
 
 
+void readRFID()
+{
+  rfid.PICC_ReadCardSerial(); // Считываем данные
+
+  Serial.println("Scanned PICC's UID:"); 
+  id_str= String(rfid.uid.uidByte[0])+" "+String(rfid.uid.uidByte[1])+" "+String(rfid.uid.uidByte[2])+ " "+String(rfid.uid.uidByte[3]); // Создаем строку с id
+  Serial.println(id_str);
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+}
+
+
+
+
+
 void setup() {
   Serial.begin(9600);
 
   button.attachClick(click1);
 
-  SPI.begin();
-	mfrc522.PCD_Init(); 
+	SPI.begin();
+	rfid.PCD_Init();
+  rfid.PCD_DumpVersionToSerial();
+	Serial.println("RFID is active");
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
-
-  Serial.println("OLED begun");
-
-  display.display();
-  delay(1000);
-
-  // Clear the buffer.
+  Serial.println("Display is active");
   display.clearDisplay();
   display.display();
-
 }
 
 void loop() {
@@ -53,5 +66,9 @@ void loop() {
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.print("CHECKING");
-  display.display(); // actually display all of the above
+  display.display();
+  
+	if (rfid.PICC_IsNewCardPresent()){ // Если увидели новую карту, то читаем 
+		readRFID();
+	}
 }
