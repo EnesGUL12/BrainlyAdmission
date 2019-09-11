fd49148e9a738f0214d702d37e2d2b4531f52aa3
Enes <enes515254@gmail.com>
2019-09-11 18:06:34 +0600

    Added password checking

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,12 @@ Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
 OneButton button(16, false);
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-byte code;
-String id_str; 
+byte code[2][4] = {
+                {101,29,31,25},
+                {45,191,19,211} // Enes
+                  };
+String id_str;
+bool flag_online;
 
 
 void click1(){
@@ -34,6 +38,26 @@ void readRFID()
   Serial.println("Scanned PICC's UID:"); 
   id_str= String(rfid.uid.uidByte[0])+" "+String(rfid.uid.uidByte[1])+" "+String(rfid.uid.uidByte[2])+ " "+String(rfid.uid.uidByte[3]); // Создаем строку с id
   Serial.println(id_str);
+
+  if(!flag_online){ // Если не активировали
+    for(int i = 0; i <= 1; i++){ // Проверяем все аккаунты
+      for(int r = 0; r <= 3;){
+        if(rfid.uid.uidByte[r] != code[i][r]){ // Если цифры не совпадают то останавливем и начинаем проверять следущий
+          break;
+        }
+        else if(rfid.uid.uidByte[r] == code[i][r]){ // Если цифры совпадают то активируем
+          r++;
+          Serial.print("num: "); Serial.print(r); Serial.println(" is true");
+          if(r >= 3){
+            flag_online = true;
+            Serial.println("Activated");
+            break;
+          }
+          
+        }
+      }
+    }
+  }
 
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
